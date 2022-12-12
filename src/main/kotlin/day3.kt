9705f524d2a57a5977d63f17d391c063bb623e06
Ethan Mills <ethan.mills@hotmail.co.uk>
2022-12-12 09:36:56 +0000

    Refactor day 3

--- src/main/kotlin/day3.kt
+++ src/main/kotlin/day3.kt
@@ -1,27 +1,25 @@
 fun day3(): Pair<Int, Int> {
-    val lowerScores = ('a'..'z').withIndex().associateBy({ it.value.toString() }, { it.index + 1 })
-    val upperScores = ('A'..'Z').withIndex().associateBy({ it.value.toString() }, { it.index + 27 })
+    val lowerScores = ('a'..'z').withIndex().associateBy({ it.value }, { it.index + 1 })
+    val upperScores = ('A'..'Z').withIndex().associateBy({ it.value }, { it.index + 27 })
     val totalScores = lowerScores.plus(upperScores)
 
     val lines = getLines("day3.txt")
-    val duplicates = lines.map { findDuplicateItem(it) }
-    val scores = duplicates.sumOf { totalScores[it] ?: 0 }
-
+    val part1 = lines.map { findDuplicateItem(it) }.sumOf { totalScores[it] ?: 0 }
     val part2 = lines.chunked(3).map { badgeFromGroup(it) }.sumOf { totalScores[it] ?: 0 }
 
 
-    return Pair(scores, part2)
+    return Pair(part1, part2)
 }
 
-fun findDuplicateItem(contents: String): String {
-    val compartmentSize = contents.count() / 2
-    val partA = contents.take(compartmentSize).split("").filterNot { it.isBlank() }.toSet()
-    val partB = contents.takeLast(compartmentSize).split("").filterNot { it.isBlank() }.toSet()
-    return partA.intersect(partB).firstOrNull() ?: ""
+fun findDuplicateItem(contents: String): Char {
+    return contents.chunked(contents.count() / 2)
+        .map(String::toSet)
+        .reduce(Set<Char>::intersect)
+        .first()
 }
 
-fun badgeFromGroup(group: List<String>): String {
-    return group.map { singleLine -> singleLine.split("").filterNot { it.isBlank() }.toSet() }
-        .reduce { setA, setB -> setA.intersect(setB) }
-        .firstOrNull() ?: throw RuntimeException()
+fun badgeFromGroup(group: List<String>): Char {
+    return group.map(String::toSet)
+        .reduce(Set<Char>::intersect)
+        .first()
 }
